2c79e081e2d40a0bb391fa047d69a2d3c58c8639
mayur krushnarao dahake <35840117+mayue13@users.noreply.github.com>
2023-01-29 12:07:18 +0530

    update

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -9,7 +9,7 @@ export default function SocialMedia (){
             <div><a href='https://www.facebook.com/profile.php?id=100089752297389' target="_blank" rel="noopener noreferrer"><BsFacebook /></a></div>
             <div><a href='https://www.instagram.com/yash_lights_official' target="_blank" rel="noopener noreferrer"><BsInstagram/></a></div>
             <div><a href='https://www.youtube.com/channel/UCaWKYx6sU0ftLXVrtXupkiA' target="_blank" rel="noopener noreferrer"><BsYoutube/></a></div>
-           
+            <div><a href='tel:7218141143' target="_blank" rel="noopener noreferrer"><BsFillTelephoneFill/></a></div>
         </div>
 
     )
